c88f94b13ccd719924fe1ad90c047433a6db2d25
christophe <christophegueroult@gmail.com>
2021-04-26 16:21:00 +0200

    config scss

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [IconsModule, TemplatesModule, LoginModule],
+  exports: [IconsModule, TemplatesModule, LoginModule, UiModule],
 })
 export class CoreModule {}
