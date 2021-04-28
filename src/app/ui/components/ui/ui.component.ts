5a24eb1c173112b35c2617dde73fa8e2cad44a23
christophe <christophegueroult@gmail.com>
2021-04-28 11:01:38 +0200

    documentation and webpack bundle analyzer

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+/**
+ * description pour la class
+ */
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
   styleUrls: ['./ui.component.scss'],
 })
 export class UiComponent implements OnInit {
+  /**
+   * un commentaire pour cette propriété
+   */
   public open = true;
   constructor() {}
 
   ngOnInit(): void {}
+  /**
+   * un commentaire pour la fn toggle
+   */
   public toggle(): void {
     this.open = !this.open;
     console.log(this.open);
