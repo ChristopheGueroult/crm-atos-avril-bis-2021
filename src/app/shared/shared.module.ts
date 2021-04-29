d468ffcf42c31914ecf855ca739fe4f397a62fbf
christophe <christophegueroult@gmail.com>
2021-04-29 14:52:49 +0200

    tp page list clients

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,10 +7,11 @@ import { BtnComponent } from './components/btn/btn.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 
 @NgModule({
-  declarations: [TableLightComponent, BtnComponent, TotalPipe, StateDirective],
+  declarations: [TableLightComponent, BtnComponent, TotalPipe, StateDirective, TableDarkComponent],
   imports: [CommonModule, RouterModule],
-  exports: [TableLightComponent, BtnComponent, IconsModule, TemplatesModule, TotalPipe, StateDirective],
+  exports: [TableLightComponent, BtnComponent, IconsModule, TemplatesModule, TotalPipe, StateDirective, TableDarkComponent],
 })
 export class SharedModule {}
