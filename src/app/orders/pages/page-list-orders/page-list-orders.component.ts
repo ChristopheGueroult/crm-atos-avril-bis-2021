97347b1ac113219aac87a086051515808a129c56
christophe <christophegueroult@gmail.com>
2021-04-29 10:38:38 +0200

    ChangeDetectorRef and detectChanges

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { StateOrder } from 'src/app/core/enums/state-order.enum';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
 
@@ -7,13 +9,23 @@ import { OrdersService } from '../../services/orders.service';
   templateUrl: './page-list-orders.component.html',
   styleUrls: ['./page-list-orders.component.scss'],
 })
-export class PageListOrdersComponent implements OnInit {
-  public collection!: Order[];
+export class PageListOrdersComponent implements OnInit, OnDestroy {
+  public states = Object.values(StateOrder);
+  private obs = new Observable((list) => {
+    list.next('string');
+  });
+  private sub!: Subscription;
+  // public collection!: Order[];
+  public collection$: Observable<Order[]>;
   public headers: string[];
-  constructor(private ordersService: OrdersService) {
-    this.ordersService.collection.subscribe((data) => {
-      this.collection = data;
-    });
+  constructor(
+    private ordersService: OrdersService,
+    private cd: ChangeDetectorRef
+  ) {
+    this.collection$ = this.ordersService.collection;
+    // this.ordersService.collection.subscribe((data) => {
+    //   this.collection = data;
+    // });
     this.headers = [
       'Type',
       'Client',
@@ -25,5 +37,21 @@ export class PageListOrdersComponent implements OnInit {
     ];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.sub = this.obs.subscribe((data) => console.log(data));
+  }
+  public changeState(item: Order, event: any): void {
+    const state = event.target.value;
+    this.ordersService.changeItem(item, state).subscribe((data) => {
+      // traiter les reponses de l api
+      item.state = data.state;
+      // besoin de lancer un change detection manuellement car la reference d'item de change pas
+      this.cd.detectChanges();
+      // item = data;
+      // ici inutile car la ref de l'item change et le change detection strategy onpush lance un CD si la ref d'item change
+    });
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
