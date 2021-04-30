ae2f8d044143fe045bb8f7197f84ae144fb5c896
christophe <christophegueroult@gmail.com>
2021-04-30 09:33:33 +0200

    router navigate

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { BtnComponent } from './components/btn/btn.component';
-import { TableLightComponent } from './components/table-light/table-light.component';
-import { TotalPipe } from './pipes/total.pipe';
-import { StateDirective } from './directives/state.directive';
 import { TableDarkComponent } from './components/table-dark/table-dark.component';
+import { TableLightComponent } from './components/table-light/table-light.component';
+import { StateDirective } from './directives/state.directive';
+import { TotalPipe } from './pipes/total.pipe';
 
 @NgModule({
-  declarations: [TableLightComponent, BtnComponent, TotalPipe, StateDirective, TableDarkComponent],
+  declarations: [
+    TableLightComponent,
+    BtnComponent,
+    TotalPipe,
+    StateDirective,
+    TableDarkComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [TableLightComponent, BtnComponent, IconsModule, TemplatesModule, TotalPipe, StateDirective, TableDarkComponent],
+  exports: [
+    TableLightComponent,
+    BtnComponent,
+    IconsModule,
+    TemplatesModule,
+    TotalPipe,
+    StateDirective,
+    TableDarkComponent,
+    ReactiveFormsModule,
+  ],
 })
 export class SharedModule {}
